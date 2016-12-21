afe02b5d8e72b06f618ea783db66b0f2f0c449be
mmidgett <wixcedperformance@gmail.com>
2016-12-20 22:10:27 -0500

    First C db connection - not working

--- src/trick_hvac.c
+++ src/trick_hvac.c
@@ -4,14 +4,47 @@
  Author      : Matthew Midgett /  Randy Gilliand
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : C program to control HVAC using Raspberry Pi
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+main() {
+
+   MYSQL *conn;
+   MYSQL_RES *res;
+   MYSQL_ROW row;
+
+   const char *server = "localhost";
+   const char *user = "swriter";
+   const char *password = "PASSWORD"; /* set me first */
+   const char *database = "hvac";
+
+   conn = mysql_init(NULL);
+
+   /* Connect to database */
+   if (!mysql_real_connect(conn, server,
+         user, password, database, 0, NULL, 0)) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);
+   }
+
+   /* send SQL query */
+   if (mysql_query(conn, "show tables")) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);#include </usr/include/mysql/mysql.h>
+   }
+
+   res = mysql_use_result(conn);
+
+   /* output table name */
+   printf("MySQL Tables in mysql database:\n");
+   while ((row = mysql_fetch_row(res)) != NULL)
+      printf("%s \n", row[0]);
+
+   /* close connection */
+   mysql_free_result(res);
+   mysql_close(conn);
 }
